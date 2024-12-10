81b0ce547469ed524dca5ddd09c42f0c0d436e6d
DELORME MAXIMILIEN <mdelor03@iut.local>
2024-12-10 15:23:10 +0100

    pour @florianamiot

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const imgs = document.querySelectorAll(".output_img");
+  const spawn_buttons = document.querySelectorAll(".spawn_button");
+
+  this.spawn_buttons.addEventListener("click", function () {
+    imgs.forEach((element) => {
+      console.log(test);
+    });
+  });
+});
