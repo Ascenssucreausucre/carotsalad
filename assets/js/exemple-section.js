b8e9a6a97b4ee77c98e65688e76a32b8550ae086
unknown <amiotflorian33@gmail.com>
2024-12-15 16:20:21 +0100

    envie de reset ma vie

--- assets/js/exemple-section.js
+++ assets/js/exemple-section.js
@@ -1,12 +1,31 @@
-class ExempleSection extends HTMLElement{
-    connectedCallback(){
-        const template = document.getElementById("exemple_section").content;
-        this.appendChild(template.cloneNode(true));
-    }
+class ExempleSection extends HTMLElement {
+  connectedCallback() {
+    const template = document.getElementById("exemple_section").content;
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(template.cloneNode(true));
+    this.initListeners(shadow);
+  }
 
-    render(){
+  initListeners(shadow) {
+    const imgs = shadow.querySelectorAll(".output_img");
+    const spawn_buttons = shadow.querySelectorAll(".spawn_button");
+    const despawn_button = shadow.querySelector(".despawn_button");
 
-    }
+    despawn_button.addEventListener("click", function () {
+      console.log(imgs);
+      imgs.forEach((img) => {
+        img.style.display = "none";
+      });
+    });
+
+    spawn_buttons.forEach((button) => {
+      button.addEventListener("click", function () {
+        const image = this.closest(".exemple").querySelector(".output_img");
+        image.style.display = "block";
+        console.log(image);
+      });
+    });
+  }
 }
 
 customElements.define("exemple-section", ExempleSection);
