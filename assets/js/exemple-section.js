abba9502c6571846ae17066832c5c408b6abcf8b
unknown <amiotflorian33@gmail.com>
2024-12-15 16:54:41 +0100

    clean example-section shadowDOM

--- assets/js/exemple-section.js
+++ assets/js/exemple-section.js
@@ -1,10 +1,30 @@
 class ExempleSection extends HTMLElement {
   connectedCallback() {
     const template = document.getElementById("exemple_section").content;
-    this.appendChild(template.cloneNode(true));
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.appendChild(template.cloneNode(true));
+
+    this.initListeners(shadow);
   }
 
-  render() {}
+  initListeners(shadow) {
+    const imgs = shadow.querySelectorAll(".spawn_imgs");
+    const spawn_buttons = shadow.querySelectorAll(".spawn_button");
+    const despawn_button = shadow.querySelector(".despawn_button");
+
+    despawn_button.addEventListener("click", function () {
+      imgs.forEach((img) => {
+        img.style.display = "none";
+      });
+    });
+
+    spawn_buttons.forEach((button) => {
+      button.addEventListener("click", function () {
+        const image = this.closest(".exemple").querySelector(".spawn_imgs");
+        image.style.display = "block";
+      });
+    });
+  }
 }
 
 customElements.define("exemple-section", ExempleSection);
--- assets/script.js
+++ assets/script.js
@@ -1,49 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const imgs = document.querySelectorAll(".output_img");
-  const spawn_buttons = document.querySelectorAll(".spawn_button");
-  const despawn_button = document.querySelector(".despawn_button");
+  function detectBrowser() {
+    const userAgent = navigator.userAgent;
 
-  despawn_button.addEventListener("click", function () {
-    imgs.forEach((img) => {
-      img.style.display = "none";
-    });
-  });
-
-  spawn_buttons.forEach((button) => {
-    button.addEventListener("click", function () {
-      const image = this.closest(".exemple").querySelector(".output_img");
-      image.style.display = "block";
-      console.log(image);
-    });
-  });
-
-function detectBrowser() {
-  const userAgent = navigator.userAgent;
-
-  if (userAgent.includes("Edg")) {
-    return "Microsoft Edge (Supporté)";
-  } else if (userAgent.includes("OPR") || userAgent.includes("Opera")) {
-    return "Opera (Supporté)";
-  } else if (userAgent.includes("Chrome")) {
-    return "Google Chrome (Supporté)";
-  } else if (userAgent.includes("Firefox")) {
-    return "Mozilla Firefox (Support partiel)";
-  } else if (userAgent.includes("Safari")) {
-    return "Safari (Supporté)";
-  } else if (userAgent.includes("MSIE") || userAgent.includes("Trident")) {
-    const navAlert = document.createElement("div");
-     navAlert.classList.add("nav_alert");
-     navAlert.innerHTML = "Votre navigateur ne supporte pas cette fonctionnalité";
-     const presentationSection = document.getElementById("presentation_section");
-     presentationSection.prepend(navAlert);
-    return "Internet Explorer (Non supporté)";
-  } else {
-    return "Navigateur inconnu";
+    if (userAgent.includes("Edg")) {
+      return "Microsoft Edge (Supporté)";
+    } else if (userAgent.includes("OPR") || userAgent.includes("Opera")) {
+      return "Opera (Supporté)";
+    } else if (userAgent.includes("Chrome")) {
+      return "Google Chrome (Supporté)";
+    } else if (userAgent.includes("Firefox")) {
+      return "Mozilla Firefox (Support partiel)";
+    } else if (userAgent.includes("Safari")) {
+      return "Safari (Supporté)";
+    } else if (userAgent.includes("MSIE") || userAgent.includes("Trident")) {
+      const navAlert = document.createElement("div");
+      navAlert.classList.add("nav_alert");
+      navAlert.innerHTML =
+        "Votre navigateur ne supporte pas cette fonctionnalité";
+      const presentationSection = document.getElementById(
+        "presentation_section"
+      );
+      presentationSection.prepend(navAlert);
+      return "Internet Explorer (Non supporté)";
+    } else {
+      return "Navigateur inconnu";
+    }
   }
-}
 
-detectBrowser();
-
-
-
+  detectBrowser();
 });
