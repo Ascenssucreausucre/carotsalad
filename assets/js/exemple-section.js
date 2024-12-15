849eb7e7cafb3eeea5256f4397e29a46c34612c5
unknown <amiotflorian33@gmail.com>
2024-12-15 16:40:00 +0100

    commit de sécurité

--- assets/js/exemple-section.js
+++ assets/js/exemple-section.js
@@ -1,31 +1,10 @@
 class ExempleSection extends HTMLElement {
   connectedCallback() {
     const template = document.getElementById("exemple_section").content;
-    const shadow = this.attachShadow({ mode: "open" });
-    shadow.appendChild(template.cloneNode(true));
-    this.initListeners(shadow);
+    this.appendChild(template.cloneNode(true));
   }
 
-  initListeners(shadow) {
-    const imgs = shadow.querySelectorAll(".output_img");
-    const spawn_buttons = shadow.querySelectorAll(".spawn_button");
-    const despawn_button = shadow.querySelector(".despawn_button");
-
-    despawn_button.addEventListener("click", function () {
-      console.log(imgs);
-      imgs.forEach((img) => {
-        img.style.display = "none";
-      });
-    });
-
-    spawn_buttons.forEach((button) => {
-      button.addEventListener("click", function () {
-        const image = this.closest(".exemple").querySelector(".output_img");
-        image.style.display = "block";
-        console.log(image);
-      });
-    });
-  }
+  render() {}
 }
 
 customElements.define("exemple-section", ExempleSection);
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,22 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const imgs = document.querySelectorAll(".output_img");
+  const spawn_buttons = document.querySelectorAll(".spawn_button");
+  const despawn_button = document.querySelector(".despawn_button");
+
+  despawn_button.addEventListener("click", function () {
+    imgs.forEach((img) => {
+      img.style.display = "none";
+    });
+  });
+
+  spawn_buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      const image = this.closest(".exemple").querySelector(".output_img");
+      image.style.display = "block";
+      console.log(image);
+    });
+  });
+
   function detectBrowser() {
     const userAgent = navigator.userAgent;
 
