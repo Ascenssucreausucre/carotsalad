b27b8ed618db790c848bb1126df4e4cf41fa71e1
mxou <maximilien.dlrm@gmail.com>
2024-12-19 23:43:47 +0100

    add web component baseline status pour les compatibilités browser

--- assets/js/acc-ueil.js
+++ assets/js/acc-ueil.js
@@ -8,6 +8,7 @@ class AccUeil extends HTMLElement{
         <h1 id="title">La fonctionnalité <strong>@starting-styles</strong> en css</h1>
         <hr width="20%" size="6" color="#FF0044" />
         <span>Déc. 2024</span>
+         <baseline-status featureid="starting-style"></baseline-status>
       </div>`
     }
 }
